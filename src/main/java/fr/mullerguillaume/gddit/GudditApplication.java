3b2df858fe828e241e7f2d061745c605fbb3a646
Guillaume Muller <guimulxxvi@live.fr>
2022-01-22 16:39:01 +0100

    implement swagger2 + bump lombok

--- src/main/java/fr/mullerguillaume/gddit/GudditApplication.java
+++ src/main/java/fr/mullerguillaume/gddit/GudditApplication.java
@@ -3,9 +3,13 @@ package fr.mullerguillaume.gddit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import fr.mullerguillaume.gddit.config.SwaggerConfiguration;
+import org.springframework.context.annotation.Import;
+
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class GudditApplication {
 
 	public static void main(String[] args) {
